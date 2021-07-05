093ac907625f8a6bcaf3ff483423c611a7b5c83d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-07-06 00:09:25 +0200

    ImageMapLinkResolver: Implement resolveLink and getLinkUrl to be able to return actual link object.

--- link/src/test/java/io/wcm/handler/link/impl/ImageMapLinkResolverImplTest.java
+++ link/src/test/java/io/wcm/handler/link/impl/ImageMapLinkResolverImplTest.java
@@ -20,7 +20,10 @@
 package io.wcm.handler.link.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,6 +31,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.day.cq.wcm.api.Page;
 
+import io.wcm.handler.link.Link;
 import io.wcm.handler.link.testcontext.AppAemContext;
 import io.wcm.handler.media.spi.ImageMapLinkResolver;
 import io.wcm.testing.mock.aem.junit5.AemContext;
@@ -38,7 +42,7 @@ class ImageMapLinkResolverImplTest {
 
   final AemContext context = AppAemContext.newAemContext();
 
-  private ImageMapLinkResolver underTest;
+  private ImageMapLinkResolver<Link> underTest;
   private Page page;
 
   @BeforeEach
@@ -54,6 +58,26 @@ class ImageMapLinkResolverImplTest {
     assertEquals("http://host", underTest.resolve("http://host", page.getContentResource()));
     assertEquals("/content/site1/en.html", underTest.resolve("/content/site1/en", page.getContentResource()));
     assertNull(underTest.resolve("/content/site1/en/invalid", page.getContentResource()));
+  }
+
+  @Test
+  void testResolveLink() {
+    assertValid("http://host", underTest.resolveLink("http://host", null, page.getContentResource()));
+    assertValid("/content/site1/en.html", underTest.resolveLink("/content/site1/en", null, page.getContentResource()));
+    assertInvalid(underTest.resolveLink("/content/site1/en/invalid", null, page.getContentResource()));
+  }
+
+  // TODO: testResolveLink with window target
+
+  private void assertValid(String expectedUrl, Link link) {
+    assertNotNull(link);
+    assertTrue(link.isValid());
+    assertEquals(expectedUrl, link.getUrl());
+  }
+
+  private void assertInvalid(Link link) {
+    assertNotNull(link);
+    assertFalse(link.isValid());
   }
 
 }
