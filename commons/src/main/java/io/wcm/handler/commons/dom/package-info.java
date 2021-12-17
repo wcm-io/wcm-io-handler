6a6b17a637291e25fd98c0f92d5e4881b1b59581
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-12-17 13:19:08 +0100

    WHAN-50 update to jdom 2.0.6.1

--- commons/src/main/java/io/wcm/handler/commons/dom/package-info.java
+++ commons/src/main/java/io/wcm/handler/commons/dom/package-info.java
@@ -20,5 +20,5 @@
 /**
  * DOM convenience objects used by handler implementations to build markup fragments for links and media.
  */
-@org.osgi.annotation.versioning.Version("1.2")
+@org.osgi.annotation.versioning.Version("1.3")
 package io.wcm.handler.commons.dom;
